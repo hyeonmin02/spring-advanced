c1f07808784176184bc05e96b3e25ac9458881f6
hyeonmin02 <hyenmin5@gmail.com>
2026-03-05 20:17:19 +0900

    refactor: LV 6 PasswordEncoderTest 불필요한 @ExtendWith, @InjectMocks 제거

--- src/test/java/org/example/expert/config/PasswordEncoderTest.java
+++ src/test/java/org/example/expert/config/PasswordEncoderTest.java
@@ -1,17 +1,12 @@
 package org.example.expert.config;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(SpringExtension.class)
 class PasswordEncoderTest {
-
-    @InjectMocks
-    private PasswordEncoder passwordEncoder;
+    // PasswordEncoder는 외부 의존성이 없는데 불필요하게 @InjectMocks를 통해 가짜 객체를 불러와서 제거해 테스트 의도를 명확하게함
+    private final PasswordEncoder passwordEncoder = new PasswordEncoder();
 
     @Test
     void matches_메서드가_정상적으로_동작한다() {
